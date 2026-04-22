ef253b78454fb5cf056b228fe71dde4ac7693135
Nisith Ranjan Hazra <nisith@Nisiths-MacBook-Air.local>
2026-04-23 00:51:48 +0530

    removing bug fixing file

--- Assignment-05/fix.py
+++
@@ -1,9 +0,0 @@
-import nbformat
-
-nb = nbformat.read("Assignment_05_prob_02.ipynb", as_version=4)
-
-# Remove widget metadata safely
-if "widgets" in nb.metadata:
-    del nb.metadata["widgets"]
-
-nbformat.write(nb, "Assignment_05_prob_02.ipynb")
